7f4f626b88d7189ea96f0184c3a2eb05a067ba8b
Jessica Swenson <jessicamswens@gmail.com>
2023-02-26 09:59:54 -0700

    finished app

---
+++ src/components/WatchList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+const WatchList = ({ watchList, removeMovie }) => {
+
+    const movieDisplay = watchList.map((movie) => {
+        return <MovieCard movie={movie} removeMovie={removeMovie} watchList={watchList} />
+    })
+
+    return (
+        <div className='watchlist'>
+            <h1>My Watchlist</h1>
+            <div className="movie-container">
+                {movieDisplay}
+            </div>
+        </div>
+    )
+}
+
+export default WatchList
